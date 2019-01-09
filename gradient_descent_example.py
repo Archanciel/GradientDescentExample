ec1333219a4ed62a451ede888aa72d9dc340bdf2
Archanciel <jp.schnyder@gmail.com>
2019-01-09 18:16:15 +0100

    added precision iteration stop

--- gradient_descent_example.py
+++ gradient_descent_example.py
@@ -23,23 +23,31 @@ def step_gradient(b_current, m_current, points, learningRate):
     new_m = m_current - (learningRate * m_gradient)
     return [new_b, new_m]
 
-def gradient_descent_runner(points, starting_b, starting_m, learning_rate, num_iterations):
+def gradient_descent_runner(points, starting_b, starting_m, learning_rate, num_iterations, precision):
     b = starting_b
     m = starting_m
+    old_b = starting_b
+    old_m = starting_m
     for i in range(num_iterations):
         b, m = step_gradient(b, m, array(points), learning_rate)
-    return [b, m]
+        if abs(old_b - b) < precision and abs(old_m - m) < precision:
+            print('Max precision ({}) reached after {} iterations'.format(precision, i + 1))
+            return b, m, i + 1
+        old_b = b
+        old_m = m
+    return [b, m, i + 1]
 
 def run():
     points = genfromtxt("data.csv", delimiter=",")
     learning_rate = 0.0001
     initial_b = 0 # initial y-intercept guess
     initial_m = 0 # initial slope guess
-    num_iterations = 1000
-    print "Starting gradient descent at b = {0}, m = {1}, error = {2}".format(initial_b, initial_m, compute_error_for_line_given_points(initial_b, initial_m, points))
-    print "Running..."
-    [b, m] = gradient_descent_runner(points, initial_b, initial_m, learning_rate, num_iterations)
-    print "After {0} iterations b = {1}, m = {2}, error = {3}".format(num_iterations, b, m, compute_error_for_line_given_points(b, m, points))
+    num_iterations = 10000
+    precision = 0.00001
+    print("Starting gradient descent at b = {0}, m = {1}, error = {2}".format(initial_b, initial_m, compute_error_for_line_given_points(initial_b, initial_m, points)))
+    print("Running...")
+    [b, m, num_iterations] = gradient_descent_runner(points, initial_b, initial_m, learning_rate, num_iterations, precision)
+    print("After {0} iterations b = {1}, m = {2}, error = {3}".format(num_iterations, b, m, compute_error_for_line_given_points(b, m, points)))
 
 if __name__ == '__main__':
     run()
